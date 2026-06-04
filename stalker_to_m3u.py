241b1d72fca5a12672d8ee092deebc9d7e502514
Love4vn <162122812+Love4vn@users.noreply.github.com>
2026-06-04 11:45:14 +0700

    Update stalker_to_m3u.py

--- stalker_to_m3u.py
+++ stalker_to_m3u.py
@@ -447,14 +447,15 @@ def generate_playlist(portals: List[Dict], output_file: str):
     """Tạo file M3U từ danh sách portal, sắp xếp toàn bộ kênh theo thứ tự ưu tiên quốc gia"""
     # ========== TỪ KHÓA LỌC ==========
     SPORTS_KEYWORDS = [
-        "sport", "sports", "football", "soccer", "tennis", "golf", "tudn", "telemundo", "usa network", "paramount+",
+        "sport", "sports", "Šport", "football", "soccer", "tennis", "golf", "tudn", "telemundo", "usa network", "paramount+",
         "motorsport", "formula 1", "f1", "hub premier", "premier league", "viaplay", "disney+ premium",
         "monomax", "astro arena", "spotv", "epl", "tsn", "la liga", "laliga", "bundesliga",
         "seriea", "serie a", "uefa", "arsenal", "aston villa", "bournemouth",
         "brentford", "brighton", "chelsea", "crystal palace", "everton", "fulham", "leeds united", "liverpool",
         "manchester city", "manchester united", "newcastle", "nottingham forest", "sunderland", "tottenham hotspur",
         "west ham united", "wolverhampton", "bayern", "bayern munich", "borussia dortmund", "bayer leverkusen", "inter milan",
-        "ac milan", "napoli", "barcelona", "real madrid", "atlético", "atletico madrid", "psg", "paris saint-germain", "olympique marseille"
+        "ac milan", "napoli", "barcelona", "real madrid", "atlético", "atletico madrid", "psg", "paris saint-germain", "olympique marseille",
+        "antena", "bnt", "hrt", "ictimai", "itv", "m6", "npo", "orf", "rai", "rsi la", "rtbf la", "rtk", "rts", "RTVE", "RTÉ", "Rustavi", "RUV Sjonvarpid", "SABC", "Sigma TV", "Slovenija TV", "STV Scotland", "UTV Northern Ireland", "VRT 1 Belgium", "ZDF Deutschland"
     ]
     EXCLUDE_KEYWORDS = [
         "baseball", "cricket", "nfl", "nhl", "rugby", "basketball", "bóng rổ",
